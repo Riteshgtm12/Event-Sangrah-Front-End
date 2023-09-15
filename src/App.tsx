626fa152ed92d99cec5da2dc816386ec8e538a8b
Riteshgtm12@ <Riteshgtm12@gmail.com>
2023-09-15 08:31:51 +0530

    Feat: Add Header And Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import EventDetail from './pages/EventDetail'
 import Profile from './pages/Profile'
+import MainLayout from './layout/MainLayout'
 
 const App = () => {
   return (
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/signup' element={<Signup/>}/>
-    <Route path='/event/:eventId' element={<EventDetail/>}/>
-    <Route path='/profile' element={<Profile/>}/>
-  </Routes>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/event/:eventId' element={<EventDetail />} />
+        <Route path='/profile' element={<Profile />} />
+      </Route>
+    </Routes>
   )
 }
 
