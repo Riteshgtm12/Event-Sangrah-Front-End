b4a543f0eb056da8dc0b92e37d29a6b1ed473203
Riteshgtm12@ <Riteshgtm12@gmail.com>
2023-09-26 13:49:57 +0530

    Make Login Page✔️

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -3,6 +3,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import MainLogo from '../../assests/MAIN-LOGO.png'
+import { Link } from 'react-router-dom'
 
 const user = {
   name: 'Tom Cook',
@@ -11,10 +12,10 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Tech', href: '#', current: true },
+  { name: 'Movie', href: '#', current: false },
+  { name: 'Music', href: '#', current: false },
+  { name: 'StandUp', href: '#', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
@@ -83,8 +84,9 @@ export default function Header() {
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-4 flex-shrink-0">
+                
+                {/* Profile dropdown - only visible when user logged in. */}
+                {/* <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
@@ -119,7 +121,32 @@ export default function Header() {
                       ))}
                     </Menu.Items>
                   </Transition>
-                </Menu>
+                </Menu> */}
+
+                {/* User Login & SingUp - only visible when user is not logged in */}
+                <div>
+                  <Link
+                    to={'/login'}
+                    className={classNames(
+                      false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'inline-flex items-center rounded-md py-2 px-3 text-sm font-medium'
+                    )}
+                    aria-current={true ? 'page' : undefined}
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to={'/signup'}
+                    className={classNames(
+                      false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'inline-flex items-center rounded-md py-2 px-3 text-sm font-medium'
+                    )}
+                    aria-current={true ? 'page' : undefined}
+                  >
+                    Sign Up
+                  </Link>
+                </div>
+                
               </div>
             </div>
             <nav className="hidden lg:flex lg:space-x-8 lg:py-2" aria-label="Global">
